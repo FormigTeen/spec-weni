5a9e8832874f3321e5d3cf9e3950152216f742df
Matheus Freitas <msformigteen@live.com>
2026-04-16 19:53:30 -0300

    feat: update gates

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/spec-weni/',
   plugins: [react()],
   build: {
     outDir: 'dist',
